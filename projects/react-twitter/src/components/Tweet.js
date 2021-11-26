27d895073947d641b3872a2894473c17d593feb2
Gustavo Morales <gustavo.morales@gmail.com>
2021-11-26 12:29:12 -0800

    Load Tweets

--- projects/react-twitter/src/components/Tweet.js
+++ projects/react-twitter/src/components/Tweet.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-export default function Tweet() {
+export default function Tweet({ title = '' }) {
   return (
     <Card className="mb-4">
       <Card.Body>
         <Card.Title>@gmoralesc</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">1 minute ago</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Text>{title}</Card.Text>
         <Card.Link href="#">0 likes</Card.Link>
         <Card.Link href="#">0 comments</Card.Link>
       </Card.Body>
