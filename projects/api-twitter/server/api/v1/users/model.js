67379cd14d9d4c27cafe924c9fca0291de7885a8
Gustavo Morales <gustavo.morales@gmail.com>
2021-12-03 06:50:22 -0800

    Update User Schema
    - Add lastname field
    - Add fullname virtual field
    - Hide password in JSON response
    - Encrypt password on create and save
    - Add verifyPassword method

--- projects/api-twitter/server/api/v1/users/model.js
+++ projects/api-twitter/server/api/v1/users/model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { hash, compare } = require('bcryptjs');
 
 const fields = {
   username: {
@@ -8,6 +9,12 @@ const fields = {
     maxLength: 128,
   },
   name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 256,
+  },
+  lastname: {
     type: String,
     required: true,
     trim: true,
@@ -28,9 +35,49 @@ const fields = {
   },
 };
 
+const hiddenFields = ['password'];
+
 const user = new mongoose.Schema(fields, {
   timestamps: true,
+  toJSON: {
+    virtuals: true,
+  },
+  toObject: {
+    virtuals: true,
+  },
 });
+
+user
+  .virtual('fullname')
+  .get(function getFullname() {
+    return `${this.name} ${this.lastname}`;
+  })
+  .set(function setFullname(value) {
+    const [name = '', lastname = ''] = value.split(' ');
+    this.name = name;
+    this.lastname = lastname;
+  });
+
+user.methods.toJSON = function toJSON() {
+  const document = this.toObject();
+  hiddenFields.forEach((field) => {
+    if (document[field] !== undefined) {
+      delete document[field];
+    }
+  });
+  return document;
+};
+
+user.pre('save', async function save(next) {
+  if (this.isNew || this.isModified('password')) {
+    this.password = await hash(this.password, 10);
+  }
+  next();
+});
+
+user.methods.verifyPassword = function verifyPassword(value) {
+  return compare(value, this.password);
+};
 
 const model = mongoose.model('user', user);
 
