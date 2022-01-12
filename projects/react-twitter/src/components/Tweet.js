26efbe2e9f5ae771ef418b7d75fdce5d66551382
David Zuluaga <david@davidrzuluaga.com>
2022-01-12 10:48:26 -0500

    image height fix

--- projects/react-twitter/src/components/Tweet.js
+++ projects/react-twitter/src/components/Tweet.js
@@ -12,9 +12,9 @@ export default function Tweet({
 }) {
   return (
     <Card className="mb-4">
-       <Card.Img height="100" variant="top" src={photo} />
       <Card.Body>
         <Card.Title>{user.username}</Card.Title>
+       <Card.Img height="100" variant="top" src={photo} />
         <Card.Subtitle className="mb-2 text-muted">
           {createdAt ? formatDistance(new Date(), new Date(createdAt)) : ''}
         </Card.Subtitle>
