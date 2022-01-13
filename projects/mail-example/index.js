898b56b92a48f12ac7222421a0978397cfa33959
Germán Escobar <german.escobarc@gmail.com>
2022-01-13 10:19:38 -0500

    Configure Sendgrid for mail-example

--- projects/mail-example/index.js
+++ projects/mail-example/index.js
@@ -1,30 +1,25 @@
 // 1. importar la librería - npm install nodemailer
 const nodemailer = require("nodemailer")
+const sgTransport = require('nodemailer-sendgrid');
 
 async function run() {
-  let testAccount = await nodemailer.createTestAccount();
+  var options = {
+    apiKey: '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'
+  }
 
   // 2. Crear el transport (Fake - Ethereal, Gmail, Sendgrid, MailGun, Postmark)
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
+  let transporter = nodemailer.createTransport(sgTransport(options));
 
   // 3. Send Email
   let info = await transporter.sendMail({
-    from: '"German" <german@example.com>', // sender address
-    to: 'bar@example.com, baz@example.com', // list of receivers
+    from: '"German Escobar" <german.escobar@makeitreal.camp>', // sender address
+    to: 'german.escobarc@gmail.com', // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     html: "<b>Hello world?</b>", // html body
   });
 
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
 
 run()
