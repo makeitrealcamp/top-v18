df07fdf105b912c1798ca992cf428187f9102931
Germán Escobar <german.escobarc@gmail.com>
2022-01-14 10:17:49 -0500

    Use Cloudinary image transformations

--- projects/react-twitter/src/components/Tweet.js
+++ projects/react-twitter/src/components/Tweet.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { formatDistance } from 'date-fns';
+import {AdvancedImage} from '@cloudinary/react';
+import {Cloudinary} from "@cloudinary/url-gen";
+import { fill } from "@cloudinary/url-gen/actions/resize";
 
 export default function Tweet({
   content = '',
@@ -10,11 +13,20 @@ export default function Tweet({
   likes = 0,
   photo = ''
 }) {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME
+    }
+  });
+
+  const image = cld.image(photo);
+  image.resize(fill().height(100).width(500))
+
   return (
     <Card className="mb-4">
       <Card.Body>
         <Card.Title>{user.username}</Card.Title>
-       <Card.Img height="100" variant="top" src={photo} />
+        <AdvancedImage cldImg={image} className="card-img-top" />
         <Card.Subtitle className="mb-2 text-muted">
           {createdAt ? formatDistance(new Date(), new Date(createdAt)) : ''}
         </Card.Subtitle>
