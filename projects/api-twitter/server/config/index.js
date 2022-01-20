5a8732788704bd9ed21673d5b0ef4876f5c126db
Augusto Forero <a.forarc@gmail.com>
2022-01-19 19:16:47 -0500

    Remove comments in spanish and move API key to .env

--- projects/api-twitter/server/config/index.js
+++ projects/api-twitter/server/config/index.js
@@ -6,30 +6,33 @@ const config = {
     protocol: process.env.DATABASE_PROTOCOL,
     url: process.env.DATABASE_URL,
     username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
+    password: process.env.DATABASE_PASSWORD
   },
   pagination: {
     limit: 10,
     skip: 0,
-    page: 1,
+    page: 1
   },
   sort: {
     sortBy: {
       fields: ['createdAt', 'updatedAt'],
-      default: 'createdAt',
+      default: 'createdAt'
     },
     direction: {
       default: 'desc',
-      options: ['asc', 'desc'],
-    },
+      options: ['asc', 'desc']
+    }
   },
   token: {
     secret: process.env.TOKEN_SECRET,
-    expires: process.env.TOKEN_EXPIRES,
+    expires: process.env.TOKEN_EXPIRES
   },
   cors: {
-    origin: process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
+    origin: process.env.ACCESS_CONTROL_ALLOW_ORIGIN
   },
+  email: {
+    sendgridApiKey: process.env.SENDGRID_API_KEY
+  }
 };
 
 module.exports = config;
--- projects/api-twitter/server/utils/email.js
+++ projects/api-twitter/server/utils/email.js
@@ -1,21 +1,18 @@
-// 1. importar la librería - npm install nodemailer
 const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid');
+const config = require('../config');
 
 exports.run = async (email, name) => {
   var options = {
-    apiKey:
-      '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'
+    apiKey: config.email.sendgridApiKey
   };
 
-  // 2. Crear el transport (Fake - Ethereal, Gmail, Sendgrid, MailGun, Postmark)
   let transporter = nodemailer.createTransport(sgTransport(options));
 
-  // 3. Send Email
   let info = await transporter.sendMail({
-    from: '"Jose Herrera" <jherrera.fundacioncerteza@gmail.com>', // sender address
-    to: `${email}`, // list of receivers
-    subject: 'Registro exitoso', // Subject line
-    text: `Hola, ${name}, bienvenido! Prueba realizada por Jose y Augusto.` // plain text body
+    from: '"Jose Herrera" <jherrera.fundacioncerteza@gmail.com>',
+    to: `${email}`,
+    subject: 'Registro exitoso',
+    text: `Hola, ${name}, bienvenido!`
   });
 };
